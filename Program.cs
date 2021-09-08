4432a9ad3a7b74294e05b2558770c3f3726cb540
Kipp Minton <luciankippminton@gmail.com>
2021-09-08 13:45:23 -0500

    functionality to add new user member

--- Program.cs
+++ Program.cs
@@ -41,7 +41,17 @@ namespace SealTeamSix
                 }
             };
 
-            Console.WriteLine(rolodex);
+            Console.WriteLine("Your contacts:");
+
+            foreach (IRobber robber in rolodex)
+            {
+            Console.WriteLine(robber.Name);
+            }
+
+            Console.WriteLine($"There are {rolodex.Count} robbers in your rolodex!");
+
+            Console.WriteLine("---------------");
+
             Console.WriteLine("Please enter a name for a new member!");
             string newMemberName = Console.ReadLine();
 
@@ -51,19 +61,41 @@ namespace SealTeamSix
             3. Lock Specialist (Cracks Vaults)
             ");
 
-
             int newMemberSpecialty = int.Parse(Console.ReadLine());
-
 
             Console.WriteLine("Please enter a skill level between 1-100");
             int newMemberSkillLevel = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter a percentage cut between 1-100");
             int newMemberPercentageCut = int.Parse(Console.ReadLine());           
-
             
+            IRobber newMember;
+            if (newMemberSpecialty ==  1) 
+            {
+                newMember = new Hacker(){
+                    Name = newMemberName,
+                    SkillLevel = newMemberSkillLevel,
+                    PercentageCut = newMemberPercentageCut
+                };
+                rolodex.Add(newMember);
+            } else if (newMemberSpecialty == 2) 
+            {
+                newMember = new Muscle(){
+                    Name = newMemberName,
+                    SkillLevel = newMemberSkillLevel,
+                    PercentageCut = newMemberPercentageCut
+                };
+                rolodex.Add(newMember);
+            } else if (newMemberSpecialty == 3) {
+                newMember = new LockSpecialist(){
+                    Name = newMemberName,
+                    SkillLevel = newMemberSkillLevel,
+                    PercentageCut = newMemberPercentageCut
+                };
+                rolodex.Add(newMember);
+            }
 
-
+            Console.WriteLine(rolodex.Count);
         }
     }
 }
